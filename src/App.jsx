e30777aaa1d990b749acad0e13dbe231d3e05901
dachishengelia <dachishengelia9@gmail.com>
2024-11-02 16:26:43 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <form className='form'>
+      <div className='irakli'> 
+        <h1>name:irakli</h1>
+        <h1>surname:iraklishvili</h1>
+        <h1>age:18</h1>
+        <h1>birth date: 31.12</h1>
+      </div>
+      </form>
+    </>
+  )
+}
+
+export default App
